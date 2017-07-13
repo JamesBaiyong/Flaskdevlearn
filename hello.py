2d7ebc9d33e4aa9eafb195787e754b1200682ce5
bai <gezipaopao@163.com>
2017-07-13 13:16:19 +0800

    6a

--- hello.py
+++ hello.py
@@ -23,11 +23,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['MAIL_SERVER'] = 'smtp.163.com'
 app.config['MAIL_PORT'] = '465'
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flsak]'
-app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <gezopaopao@163.com>'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <gezipaopao@163.com>'
 app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 
@@ -38,7 +38,6 @@ moment=Moment(app)
 db = SQLAlchemy(app)
 migrate=Migrate(app,db)
 mail = Mail(app)
-
 
 class Role(db.Model):
 	__tablename__ = 'roles'
