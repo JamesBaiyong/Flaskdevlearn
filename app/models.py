e8a9de642790c9b72cafa3bc645a7329e49b2c49
bai <gezipaopao@163.com>
2017-07-20 17:47:53 +0800

    10c add userphto

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
+import hashlib
 from flask_login import UserMixin,AnonymousUserMixin
 from . import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app,request
 from datetime import datetime
 
 class Permission:
@@ -145,6 +146,16 @@ class User(UserMixin,db.Model):
 	self.last_seen = datetime.utcnow()
 	db.session.add(self)
 
+    def gravatar(self,size=100,default='identicon',rating='g'):
+	if request.is_secure:
+		url = 'https://secure.gravatar.com/avatar'
+	else:
+		url = 'http://www.gravatar.com/avatar'
+	hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+	return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+		url=url,hash=hash, size=size, default=default, rating=rating)
+
+	
     def __repr__(self):
         return '<User %r>' % self.username
 
