b727606e96e874ddf8a7d228eeece32be0591358
bai <gezipaopao@163.com>
2017-07-24 19:19:27 +0800

    12b follwers

--- app/models.py
+++ app/models.py
@@ -50,7 +50,7 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 class Follow(db.Model):
-    __tablename__='follwers'
+    __tablename__='follows'
     follower_id = db.Column(db.Integer,db.ForeignKey('users.id'),primary_key=True)
     followed_id = db.Column(db.Integer,db.ForeignKey('users.id'),primary_key=True)
     timestamp = db.Column(db.DateTime,default=datetime.utcnow)
@@ -209,10 +209,12 @@ class User(UserMixin,db.Model):
 	if not self.is_following(user):
 		f = Follow(follower=self,followed=user)
 		db.session.add(f)
+
     def unfollow(self,user):
 	f = self.followed.filter_by(followed_id=user.id).first()
 	if f:
 		self.followed.remove(f)
+
     def is_following(self,user):
 	return self.followed.filter_by(followed_id=user.id).first() is not None
 
