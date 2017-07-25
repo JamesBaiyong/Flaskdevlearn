0825e27fd85996b3a188e497f9169d103b72b1df
bai <gezipaopao@163.com>
2017-07-25 10:00:00 +0800

    12e show self post

--- app/models.py
+++ app/models.py
@@ -105,6 +105,13 @@ class User(UserMixin,db.Model):
 			db.session.commit()
 		except IntegrityError:
 			db.session.rollback()
+    @staticmethod
+    def add_self_follows():
+	for user in User.query.all():
+		if not user.is_following(user):
+			user.follow(user)
+			db.session.add(user)
+			db.session.commit()
    
  
     def __init__(self,**kwargs):
@@ -117,6 +124,7 @@ class User(UserMixin,db.Model):
 	if self.email is not None and self.avatar_hash is None:
 		self.avatar_hash = hashlib.md5(
 			self.email.encode('utf-8')).hexdigest()
+	self.followed.append(Follow(followed=self))
 
 
     @property
